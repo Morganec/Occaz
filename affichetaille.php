208be1b3585e10b6f4c5678df8c343d4fa1b975c
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-23 20:57:17 +0200

    selectid

--- affichetaille.php
+++ affichetaille.php
@@ -1,18 +1,70 @@
 <?php
 require_once('connection.php');
-echo $_GET['ty'];
-if($_GET['ta']==0 && $_GET['ty']==0 && $_GET['p'];){
+
+if($_GET['ta']==0 && $_GET['ty']==0 && $_GET['p']==0){
 	$sql="SELECT * FROM Vetement";
 }else {
-	$sql="SELECT * FROM Vetement where idTaille=".$_GET['ta']."";
+	$sql="SELECT * FROM Vetement ";
+	$where=false;
+	if($_GET['ta']!=0){
+		if ($where==false){
+			$sql=$sql."WHERE idTaille =".$_GET['ta'];
+			$where=true;
+		}
+		else {
+			$sql=$sql." AND idTaille =".$_GET['ta'];
+		}
+	}
+	if($_GET['ty']!=0){
+		if ($where==false){
+			$sql=$sql."WHERE idType =".$_GET['ty'];
+			$where=true;
+		}
+		else {
+			$sql=$sql." AND idType =".$_GET['ty'];
+		}
+	}
+	if($_GET['p']!=0){
+		if ($where==false){
+			switch($_GET['p']){
+				case 1:
+				$sql=$sql."WHERE prix =1";
+				break;
+				case 2:
+				$sql=$sql."WHERE prix <=2";
+				break;
+				case 3:
+				$sql=$sql."WHERE prix BETWEEN 2 and 5";
+				break;
+				case 4:
+				$sql=$sql."WHERE prix >= 5";
+				break;
+			}
+			
+			$where=true;
+		}
+		else {
+			switch($_GET['p']){
+				case 1:
+				$sql=$sql." AND prix =1";
+				break;
+				case 2:
+				$sql=$sql." AND prix <=2";
+				break;
+				case 3:
+				$sql=$sql." AND prix BETWEEN 2 and 5";
+				break;
+				case 4:
+				$sql=$sql." AND prix >= 5";
+				break;
+			}
+			
+		}
+	}
+
 }
 
 $result =$mysqli->query($sql);
-
-
-
-/*IL EST TEMPS DE FAIRE DES SECLECT 
-}*/
 
 
 
