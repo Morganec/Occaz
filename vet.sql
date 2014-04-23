f66065d2e109415c09be99c1ceabb85ab930df43
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-23 21:19:33 +0200

    sqlGenre

--- vet.sql
+++ vet.sql
@@ -35,7 +35,7 @@ CREATE TABLE `Type` (
 
 LOCK TABLES `Type` WRITE;
 /*!40000 ALTER TABLE `Type` DISABLE KEYS */;
-INSERT INTO `Type` VALUES (1,'robes'),(2,'pulls'),(3,'pantalons'),(4,'vestes'),(5,'Tops'),(6,'chaussures'),(7,'pijamas');
+INSERT INTO `Type` VALUES (1,'robes'),(2,'pulls'),(3,'pantalons'),(4,'vestes'),(5,'Tops'),(6,'pijamas'),(7,'sac à dos'),(8,'sac à main');
 /*!40000 ALTER TABLE `Type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,6 +68,33 @@ LOCK TABLES `Taille` WRITE;
 INSERT INTO `Taille` VALUES (1,'34'),(2,'36'),(3,'38'),(4,'40'),(5,'42'),(6,'44'),(7,'46');
 /*!40000 ALTER TABLE `Taille` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Genre`
+--
+
+DROP TABLE IF EXISTS `Genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Genre` (
+  `idGenre` int(11) NOT NULL AUTO_INCREMENT,
+  `nomGenre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idGenre`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Genre`
+--
+
+
+
+
+LOCK TABLES `Genre` WRITE;
+/*!40000 ALTER TABLE `Genre` DISABLE KEYS */;
+INSERT INTO `Genre` VALUES (1,'femme'),(2,'homme'),(3,'sac');
+/*!40000 ALTER TABLE `Genre` ENABLE KEYS */;
+UNLOCK TABLES;
 --
 -- Table structure for table `Vetement`
 --
@@ -80,6 +107,7 @@ CREATE TABLE `Vetement` (
   `nomVet` varchar(50) DEFAULT NULL,
   `idType` int(11) NOT NULL,
   `idTaille` int(11) NOT NULL,
+  `idGenre` int(11) NOT NULL,
   `marque` boolean,
   `matiere` varchar(50) DEFAULT NULL,
   `couleur` varchar(50) DEFAULT NULL,
@@ -98,7 +126,7 @@ CREATE TABLE `Vetement` (
 
 LOCK TABLES `Vetement` WRITE;
 
-INSERT INTO `Vetement` VALUES (null,'robe d\'ete',1,1,false,'coton','rouge',3,'img1robe.jpg','img1robeD.jpg'),(null,'robe de soirée',1,2,true,'soie','bleu',5,'img2robe.jpg','img2robeD.jpg'),(null,'robe decontracte',1,4,false,'laine','vert',4,'img3robe.jpg','img3robeD.jpg'),(null,'pull doux',2,6,false,'laine','blanc',4,'img1pull.jpg',NULL),(null,'pull chaud',2,4,true,'laine','noir',4,'img2pull.jpg',NULL),(null,'pull troue',2,1,true,'cotton','rouge',1,'img3pull.jpg',NULL),(null,'jean confortable',3,2,true,'cotton','bleu',1,'img1jean.jpg',NULL),(null,'jean confortable',3,6,false,'cotton','bleu',1,'img2jean.jpg',NULL),(null,'jean confortable',3,5,false,'cotton','bleu',1,'img3jean.jpg',NULL),(null,'pull leopard',2,1,true,'synthetique','rose blanc',5,'img4pull.jpg',NULL);
+INSERT INTO `Vetement` VALUES (null,'robe d\'ete',1,1,1,false,'coton','rouge',3,'img1robe.jpg','img1robeD.jpg'),(null,'robe de soirée',1,2,1,true,'soie','bleu',5,'img2robe.jpg','img2robeD.jpg'),(null,'robe decontracte',1,4,1,false,'laine','vert',4,'img3robe.jpg','img3robeD.jpg'),(null,'pull doux',2,6,1,false,'laine','blanc',4,'img1pull.jpg',NULL),(null,'pull chaud',2,4,1,true,'laine','noir',4,'img2pull.jpg',NULL),(null,'pull troue',2,1,1,true,'cotton','rouge',1,'img3pull.jpg',NULL),(null,'jean confortable',3,2,2,true,'cotton','bleu',1,'img1jean.jpg',NULL),(null,'jean confortable',3,6,2,false,'cotton','bleu',1,'img2jean.jpg',NULL),(null,'jean confortable',3,5,2,false,'cotton','bleu',1,'img3jean.jpg',NULL),(null,'pull leopard',2,1,1,true,'synthetique','rose blanc',5,'img4pull.jpg',NULL);
 
 UNLOCK TABLES;
 
