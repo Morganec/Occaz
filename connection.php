a5914c255314d9e35af1184223e3eff4b7cb4eda
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-23 17:01:00 +0200

    sql

--- connection.php
+++ connection.php
@@ -1,24 +1,4 @@
 
-<!DOCTYPE html>  
- <html>
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" media="screen" type="text/css" href="vet.css"/>
-        <script type="text/javascript" src="fonction.js"></script>
-        <script src="//code.jquery.com/jquery-1.9.1.js"></script>
-        <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-
-        <script>
-    function alertzoom() {
-        var form = $("#imgb");
-    $( "#imgb" ).dialog({beforeClose: function(event, ui ) {location.reload(true);}});
-    }
-    </script>
-
-    </head>
-   
-
-<body>
 <?php
     $serveur="localhost";
     $user="root";
